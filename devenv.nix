82da8fe687ad6413db24c26b4f7d2e093888894d
Linus Yamashita <linus@yaraku.com>
2023-10-09 10:57:16 +0900

    Use latest version of bun

--- devenv.nix
+++ devenv.nix
@@ -1,10 +1,5 @@
 { pkgs, ... }:
 
-let
-  bun = pkgs.bun.overrideAttrs (oldAttrs: rec {
-        version = "0.7.3";
-      });
-in
 {
   packages = with pkgs; [ bun ];
   languages.typescript.enable = true;
